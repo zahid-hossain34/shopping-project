faffd008a4bbdd7c64e19d486bacc50c2482de4c
unknown <zahid-hossain34>
2023-08-17 15:48:45 +0600

    created all component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      HeaderComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    HeaderComponent,
+    RecipesComponent,
+    RecipesDetailsComponent,
+    RecipesListComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
+    RecipeItemComponent,
   ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
