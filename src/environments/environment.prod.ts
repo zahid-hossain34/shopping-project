03c56b8f64ff89b4feba7abf7f6ab361b8e2b805
unknown <zahid-hossain34>
2023-08-30 13:16:38 +0600

    environment setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    API_URL: 'https://recipe-book-510ca-default-rtdb.firebaseio.com/',
+    FIREBASE_ATHUNTICATION_URL: 'https://identitytoolkit.googleapis.com/v1/',
+    FIRE_BASE_API_KEY: 'AIzaSyAvajm8rYdYryPEa70IWXFv_dnk8ofMgi8',
+  };
+  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
+  production: false,
+
   API_URL: 'https://recipe-book-510ca-default-rtdb.firebaseio.com/',
   FIREBASE_ATHUNTICATION_URL: 'https://identitytoolkit.googleapis.com/v1/',
   FIRE_BASE_API_KEY: 'AIzaSyAvajm8rYdYryPEa70IWXFv_dnk8ofMgi8',
